a5a1c792d8222b5ed627653c928247e18cd7b3a4
Guilherme <gui.crb0@gmail.com>
2023-09-05 14:11:52 -0300

    Only one controller

---
+++ projeto_getx_desempenho/build/app/generated/source/buildConfig/release/com/guilhermecallandprojects/projeto_getx_desempenho/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.guilhermecallandprojects.projeto_getx_desempenho;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.guilhermecallandprojects.projeto_getx_desempenho";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
